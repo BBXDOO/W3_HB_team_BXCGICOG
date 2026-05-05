f18392b944f6f59f2f46123289734918c110a1c6
BBX19 <xizzix19@gmail.com>
2026-05-05 23:11:36 +0700

    Update entry.py
    Signed-off-by: BBX19 <xizzix19@gmail.com>

--- SYSTEM/TESTS/mpcp/runtime/entry.py
+++ SYSTEM/TESTS/mpcp/runtime/entry.py
@@ -1,4 +1,23 @@
-from mpcp.runtime.executor import run
+from mpcp.runtime.executor import run, register
+
+
+class DesignModew:
+    def __init__(self):
+        self.ctx = {}
+
+    def set_context(self, k, v):
+        self.ctx[k] = v
+
+    def run(self):
+        return {
+            "state": "SUCCESS",
+            "result": "design_done"
+        }
+
+
+# bind TASK -> MODEW
+register("design", DesignModew)
+
 
 if __name__ == "__main__":
     result = run("TASK:design")
