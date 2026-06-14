61d0e508773b94c473659c3d6c6c636882b97602
BBX19 <xizzix19@gmail.com>
2026-06-14 12:04:09 +0700

    Export local EventField and Logic27 helpers

--- core/runtime/w3lgu_mfc_logic/__init__.py
+++ core/runtime/w3lgu_mfc_logic/__init__.py
@@ -4,10 +4,20 @@ from .redr_mfc_logic import classify_event
 from .psp2_mfc_logic import route_package
 from .dtml_mfc_logic import trace_decision
 from .lrc2_mfc_logic import checkpoint_lifecycle
+from .event_field import EventField, build_event_field, event_field_from_mapping
+from .logic27_registry import Logic27Slot, get_logic_slot, iter_logic_slots
+from .logic27_selector import select_logic27
 
 __all__ = [
     "classify_event",
     "route_package",
     "trace_decision",
     "checkpoint_lifecycle",
+    "EventField",
+    "build_event_field",
+    "event_field_from_mapping",
+    "Logic27Slot",
+    "get_logic_slot",
+    "iter_logic_slots",
+    "select_logic27",
 ]
