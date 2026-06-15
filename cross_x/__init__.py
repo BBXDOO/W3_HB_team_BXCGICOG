bbae5adcc8fb234f537461464c235dd416c210e4
BBX19 <xizzix19@gmail.com>
2026-06-15 20:12:32 +0700

    feat(ecs): export return binding helpers

--- cross_x/__init__.py
+++ cross_x/__init__.py
@@ -1,13 +1,25 @@
 """Cross-X ecosystem coordination package."""
 
 from cross_x.core import CrossXPlan, CrossXRequest, build_cross_x_plan
-from cross_x.event_chain import EventChain, EventChainRecord, build_event_chain
+from cross_x.event_chain import (
+    EventChain,
+    EventChainRecord,
+    bind_event_return,
+    build_event_chain,
+    freeze_ecs_mapping,
+    normalize_ecs_identifier,
+    thaw_ecs_mapping,
+)
 
 __all__ = [
     "CrossXPlan",
     "CrossXRequest",
     "EventChain",
     "EventChainRecord",
+    "bind_event_return",
     "build_cross_x_plan",
     "build_event_chain",
+    "freeze_ecs_mapping",
+    "normalize_ecs_identifier",
+    "thaw_ecs_mapping",
 ]
